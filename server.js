7bb385cc01233982aaf16bea814d99365a8d6695
CHRISTO GOGLER <christogogler@gmail.com>
2021-06-30 21:00:32 +0200

    setup server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { getImages } = require("./db");
+const app = express();
+const PORT = 9090;
+
+app.use(express.static(path.join(__dirname + "/public")))
+    .use(express.urlencoded({ extended: false }))
+
+    .get("/api/images.json", (request, response) => {
+        console.log("(GET /api/images.json)");
+        getImages()
+            .then((images) => {
+                response.json(images);
+            })
+            .catch((error) => {
+                console.log("Error getting images from db: ", error);
+            });
+    })
+    .listen(PORT, () => console.log(`...listening on PORT ${PORT}`));
