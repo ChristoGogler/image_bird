9459e599eaa7c615d0997932ab4031dc6110e4c1
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-05 15:26:01 +0200

    add table comments

--- sql/images.sql
+++ sql/images.sql
@@ -1,4 +1,8 @@
+DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS images;
+
+
+
 
 CREATE TABLE images(
     id SERIAL PRIMARY KEY,
@@ -6,6 +10,14 @@ CREATE TABLE images(
     username VARCHAR NOT NULL,
     title VARCHAR NOT NULL,
     description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE comments(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR NOT NULL,
+    comment TEXT,
+    img_id INTEGER NOT NULL REFERENCES images (id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
