d40adbe1fd7a1d8c14589f48258085ca7067f484
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-07 00:20:04 +0200

    heroku commit

--- sql/images.sql
+++ sql/images.sql
@@ -1,9 +1,6 @@
 DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS images;
 
-
-
-
 CREATE TABLE images(
     id SERIAL PRIMARY KEY,
     url VARCHAR NOT NULL,
@@ -19,25 +16,4 @@ CREATE TABLE comments(
     comment TEXT,
     img_id INTEGER NOT NULL REFERENCES images (id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-INSERT INTO images (url, username, title, description) VALUES (
-    'https://s3.amazonaws.com/imageboard/jAVZmnxnZ-U95ap2-PLliFFF7TO0KqZm.jpg',
-    'funkychicken',
-    'Welcome to Spiced and the Future!',
-    'This photo brings back so many great memories.'
-);
-
-INSERT INTO images (url, username, title, description) VALUES (
-    'https://s3.amazonaws.com/imageboard/wg8d94G_HrWdq7bU_2wT6Y6F3zrX-kej.jpg',
-    'discoduck',
-    'Elvis',
-    'We can''t go on together with suspicious minds.'
-);
-
-INSERT INTO images (url, username, title, description) VALUES (
-    'https://s3.amazonaws.com/imageboard/XCv4AwJdm6QuzjenFPKJocpipRNNMwze.jpg',
-    'discoduck',
-    'To be or not to be',
-    'That is the question.'
 );
